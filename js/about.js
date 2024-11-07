e4f9235e1c07fadb66495e165d2bd62155cb6f56
Egor Grebenuk <107270919+Gorienich@users.noreply.github.com>
2024-11-07 18:31:53 +0200

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
   // Handle progress bars
   const progressBars = document.querySelectorAll('.progress');
   progressBars.forEach(bar => {
