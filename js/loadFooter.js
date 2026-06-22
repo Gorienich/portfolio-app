319d470c4e487fe504b59bb7f076e6a9bacb6fdc
Freelance / Independent <107270919+Gorienich@users.noreply.github.com>
2026-06-22 22:22:19 +0300

    Delete js directory

--- js/loadFooter.js
+++
@@ -1,9 +0,0 @@
-fetch('footer.html')
-  .then(response => response.text())
-  .then(data => {
-    const footer = document.getElementById('global-footer');
-    if (footer) {
-      footer.innerHTML = data;
-    }
-  })
-  .catch(error => console.error('Error loading footer:', error));
